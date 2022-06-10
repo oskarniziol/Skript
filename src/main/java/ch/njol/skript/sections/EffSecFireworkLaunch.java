99be1686d2453efbadb35cc224ef2a89dbeb31e0
_Mads <75088349+TFSMads@users.noreply.github.com>
2022-06-10 18:07:56 +0200

    Update EffSecFireworkLaunch.java

--- src/main/java/ch/njol/skript/sections/EffSecFireworkLaunch.java
+++ src/main/java/ch/njol/skript/sections/EffSecFireworkLaunch.java
@@ -50,7 +50,7 @@ import java.util.List;
 @Description({"Launch firework effects at the given location(s).",
 	"This can be used as an effect and as a section.",
 	"If it is used as a section, the section is run before the entity is added to the world.",
-	"You can modify the firework in this section, using for example 'event-entity' or 'event-firework'. ",
+	"You can modify the firework in this section, using for example 'event-entity' or 'event-firework'.",
 	"Do note that other event values, such as 'player', won't work in this section."})
 @Examples({"launch ball large coloured red, purple and white fading to light green and black at player's location with duration 1",
 	"",
@@ -95,11 +95,11 @@ public class EffSecFireworkLaunch extends EffectSection {
 		}, 0);
 	}
 
-	@SuppressWarnings("null")
+	@SuppressWarnings("NotNullFieldNotInitialized")
 	private Expression<FireworkEffect> effects;
-	@SuppressWarnings("null")
+	@SuppressWarnings("NotNullFieldNotInitialized")
 	private Expression<Location> locations;
-	@SuppressWarnings("null")
+	@SuppressWarnings("NotNullFieldNotInitialized")
 	private Expression<Number> lifetime;
 
 	@Nullable
