50b28cef8819805eee790d7a4254e6f6f08395cb
TheLimeGlass <seantgrover@gmail.com>
2023-04-12 23:29:21 -0600

    Move classes

--- src/main/java/ch/njol/skript/expressions/ExprVectorXYZ.java
+++ src/main/java/ch/njol/skript/expressions/ExprVectorXYZ.java
@@ -21,9 +21,12 @@ package ch.njol.skript.expressions;
 import org.bukkit.event.Event;
 import org.bukkit.util.Vector;
 import org.eclipse.jdt.annotation.Nullable;
+import org.joml.Quaternionf;
 
+import ch.njol.skript.Skript;
 import ch.njol.skript.classes.Changer;
 import ch.njol.skript.classes.Changer.ChangeMode;
+import ch.njol.skript.classes.Changer.ChangerUtils;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
@@ -35,91 +38,157 @@ import ch.njol.util.Kleenean;
 import ch.njol.util.coll.CollectionUtils;
 
 /**
+ * Ported by Sashie from skript-vectors with bi0qaw's permission.
  * @author bi0qaw
  */
-@Name("Vectors - XYZ Component")
-@Description("Gets or changes the x, y or z component of a vector.")
-@Examples({"set {_v} to vector 1, 2, 3",
-		"send \"%x of {_v}%, %y of {_v}%, %z of {_v}%\"",
-		"add 1 to x of {_v}",
-		"add 2 to y of {_v}",
-		"add 3 to z of {_v}",
-		"send \"%x of {_v}%, %y of {_v}%, %z of {_v}%\"",
-		"set x component of {_v} to 1",
-		"set y component of {_v} to 2",
-		"set z component of {_v} to 3",
-		"send \"%x component of {_v}%, %y component of {_v}%, %z component of {_v}%\"",})
-@Since("2.2-dev28")
-public class ExprVectorXYZ extends SimplePropertyExpression<Vector, Number> {
-	
+@Name("Vector/Quaternion - XYZ Component")
+@Description("Gets or changes the x, y or z component of <a href='classes.html#vector'>vectors</a>/<a href='classes.html#quaternion'>quaternions</a>.")
+@Examples({
+	"set {_v} to vector 1, 2, 3",
+	"send \"%x of {_v}%, %y of {_v}%, %z of {_v}%\"",
+	"add 1 to x of {_v}",
+	"add 2 to y of {_v}",
+	"add 3 to z of {_v}",
+	"send \"%x of {_v}%, %y of {_v}%, %z of {_v}%\"",
+	"set x component of {_v} to 1",
+	"set y component of {_v} to 2",
+	"set z component of {_v} to 3",
+	"send \"%x component of {_v}%, %y component of {_v}%, %z component of {_v}%\""
+})
+@Since("2.2-dev28, INSERT VERSION (Quaternions)")
+public class ExprVectorXYZ extends SimplePropertyExpression<Object, Number> {
+
 	static {
-		register(ExprVectorXYZ.class, Number.class, "[vector] (0¦x|1¦y|2¦z) [component[s]]", "vectors");
+		String types = "vectors";
+		if (Skript.isRunningMinecraft(1, 19, 4))
+			types += "/quaternions";
+		register(ExprVectorXYZ.class, Number.class, "[vector|quaternion] (0¦x|1¦y|2¦z) [component[s]]", types);
 	}
-	
+
 	private final static Character[] axes = new Character[] {'x', 'y', 'z'};
-	
+
 	private int axis;
-	
+
 	@Override
 	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
 		super.init(exprs, matchedPattern, isDelayed, parseResult);
 		axis = parseResult.mark;
 		return true;
 	}
-	
+
 	@Override
-	public Number convert(Vector v) {
-		return axis == 0 ? v.getX() : (axis == 1 ? v.getY() : v.getZ());
+	public Number convert(Object object) {
+		if (object instanceof Vector) {
+			Vector vector = (Vector) object;
+			return axis == 0 ? vector.getX() : (axis == 1 ? vector.getY() : vector.getZ());
+		} else {
+			Quaternionf quaternion = (Quaternionf) object;
+			switch (axis) {
+				case 0:
+					return quaternion.w();
+				case 1:
+					return quaternion.x();
+				case 2:
+					return quaternion.y();
+				case 3:
+					return quaternion.z();
+				default:
+					return null;
+			}
+		}
 	}
-	
+
 	@Override
-	@SuppressWarnings("null")
 	public Class<?>[] acceptChange(ChangeMode mode) {
+		if (getExpr().getReturnType().equals(Quaternionf.class)) {
+			if ((mode == ChangeMode.SET || mode == ChangeMode.ADD || mode == ChangeMode.REMOVE))
+				return new Class[] {Number.class};
+		}
 		if ((mode == ChangeMode.ADD || mode == ChangeMode.REMOVE || mode == ChangeMode.SET)
 				&& getExpr().isSingle() && Changer.ChangerUtils.acceptsChange(getExpr(), ChangeMode.SET, Vector.class))
 			return CollectionUtils.array(Number.class);
 		return null;
 	}
-	
+
 	@Override
-	public void change(Event e, @Nullable Object[] delta, ChangeMode mode) {
+	public void change(Event event, @Nullable Object[] delta, ChangeMode mode) {
 		assert delta != null;
-		final Vector v = getExpr().getSingle(e);
-		if (v == null)
-			return;
-		double n = ((Number) delta[0]).doubleValue();
-		switch (mode) {
-			case REMOVE:
-				n = -n;
-				//$FALL-THROUGH$
-			case ADD:
-				if (axis == 0)
-					v.setX(v.getX() + n);
-				else if (axis == 1)
-					v.setY(v.getY() + n);
-				else
-					v.setZ(v.getZ() + n);
-				getExpr().change(e, new Vector[] {v}, ChangeMode.SET);
-				break;
-			case SET:
-				if (axis == 0)
-					v.setX(n);
-				else if (axis == 1)
-					v.setY(n);
-				else
-					v.setZ(n);
-				getExpr().change(e, new Vector[] {v}, ChangeMode.SET);
+		for (Object object : getExpr().getArray(event)) {
+			if (object instanceof Vector) {
+				Vector vector = (Vector) object;
+				double value = ((Number) delta[0]).doubleValue();
+				switch (mode) {
+					case REMOVE:
+						value = -value;
+						//$FALL-THROUGH$
+					case ADD:
+						if (axis == 0)
+							vector.setX(vector.getX() + value);
+						else if (axis == 1)
+							vector.setY(vector.getY() + value);
+						else
+							vector.setZ(vector.getZ() + value);
+						getExpr().change(event, new Vector[] {vector}, ChangeMode.SET);
+						break;
+					case SET:
+						if (axis == 0)
+							vector.setX(value);
+						else if (axis == 1)
+							vector.setY(value);
+						else
+							vector.setZ(value);
+						getExpr().change(event, new Vector[] {vector}, ChangeMode.SET);
+				}
+			} else {
+				float value = ((Number) delta[0]).floatValue();
+				Quaternionf quaternion = (Quaternionf) object;
+				switch (mode) {
+					case REMOVE:
+						value = -value;
+						//$FALL-THROUGH$
+					case ADD:
+						if (axis == 0) {
+							quaternion.set(quaternion.w() + value, quaternion.x(), quaternion.y(), quaternion.z());
+						} else if (axis == 1) {
+							quaternion.set(quaternion.w(), quaternion.x() + value, quaternion.y(), quaternion.z());
+						} else if (axis == 2) {
+							quaternion.set(quaternion.w(), quaternion.x(), quaternion.y() + value, quaternion.z());
+						} else if (axis == 3) {
+							quaternion.set(quaternion.w(), quaternion.x(), quaternion.y(), quaternion.z() + value);
+						}
+						if (ChangerUtils.acceptsChange(getExpr(), ChangeMode.SET, Quaternionf.class))
+							getExpr().change(event, new Quaternionf[] {quaternion}, ChangeMode.SET);
+						break;
+					case SET:
+						if (axis == 0) {
+							quaternion.set(value, quaternion.x(), quaternion.y(), quaternion.z());
+						} else if (axis == 1) {
+							quaternion.set(quaternion.w(), value, quaternion.y(), quaternion.z());
+						} else if (axis == 2) {
+							quaternion.set(quaternion.w(), quaternion.x(), value, quaternion.z());
+						} else if (axis == 3) {
+							quaternion.set(quaternion.w(), quaternion.x(), quaternion.y(), value);
+						}
+						if (ChangerUtils.acceptsChange(getExpr(), ChangeMode.SET, Quaternionf.class))
+							getExpr().change(event, new Quaternionf[] {quaternion}, ChangeMode.SET);
+						break;
+					case DELETE:
+					case REMOVE_ALL:
+					case RESET:
+						assert false;
+				}
+			}
 		}
 	}
-	
-	@Override
-	protected String getPropertyName() {
-		return axes[axis] + " component";
-	}
-	
+
 	@Override
 	public Class<Number> getReturnType() {
 		return Number.class;
 	}
-	
+
+	@Override
+	protected String getPropertyName() {
+		return axes[axis] + " component";
+	}
+
 }
