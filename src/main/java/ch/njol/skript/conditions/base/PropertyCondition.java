d8867e9324a1fb430a5ad50165b731c841e18414
LimeGlass <16087552+TheLimeGlass@users.noreply.github.com>
2023-04-19 19:31:07 -0600

    Update src/main/java/ch/njol/skript/conditions/base/PropertyCondition.java
    Co-authored-by: Ayham Al Ali <20037329+AyhamAl-Ali@users.noreply.github.com>

--- src/main/java/ch/njol/skript/conditions/base/PropertyCondition.java
+++ src/main/java/ch/njol/skript/conditions/base/PropertyCondition.java
@@ -85,7 +85,7 @@ public abstract class PropertyCondition<T> extends Condition implements Checker<
 
 	/**
 	 * Registers a property condition. Use [!PATTERN] to set optional patterns for the negated part of the syntax.
-	 * Example <code>[!no|a] [direct] line of sight</code> this means if <code>no</code> part was used it will mark the pattern as negated.
+	 * Example <code>[[!no]|a] [direct] line of sight</code> this means if <code>no</code> part was used it will mark the pattern as negated.
 	 * 
 	 * @param c the class to register
 	 * @param property the property name, for example <i>fly</i> in <i>players can fly</i>
