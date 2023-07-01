9b73d71cddc939917f6ce95ed7fd70b93c246bb1
TheLimeGlass <seantgrover@gmail.com>
2023-06-30 23:48:41 -0600

    Include IllegalArgumentException

--- src/main/java/ch/njol/skript/effects/EffOpenInventory.java
+++ src/main/java/ch/njol/skript/effects/EffOpenInventory.java
@@ -180,7 +180,7 @@ public class EffOpenInventory extends Effect {
 			return null;
 		try {
 			return Bukkit.createInventory(null, type);
-		} catch (NullPointerException e) {
+		} catch (NullPointerException | IllegalArgumentException e) {
 			// Spigot forgot to label some InventoryType's as non creatable in some versions < 1.19.4
 			// So this throws NullPointerException aswell ontop of the IllegalArgumentException.
 			// See https://hub.spigotmc.org/jira/browse/SPIGOT-7301
