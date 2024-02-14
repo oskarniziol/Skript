26e007ab9961a5929dddac9a074ab7be084e2d53
LimeGlass <16087552+TheLimeGlass@users.noreply.github.com>
2024-02-14 00:40:28 -0700

    Update BukkitClasses.java

--- src/main/java/ch/njol/skript/classes/data/BukkitClasses.java
+++ src/main/java/ch/njol/skript/classes/data/BukkitClasses.java
@@ -1585,12 +1585,12 @@ public class BukkitClasses {
 
 						@Override
 						public String toString(DamageType type, int flags) {
-							return Classes.toString(type.getKey());
+							return type.getKey();
 						}
 
 						@Override
 						public String toVariableNameString(DamageType type) {
-							return "damage type: " + Classes.toString(type.getKey());
+							return "damage type: " + type.getKey();
 						}
 					}));
 		}
